80c920f9db747d88b98af6240f7618fce8f19182
bpv834 <yungjoon2@naver.com>
2025-06-08 20:35:05 +0900

    인기 도시 수정 매인 스크린에서 각각 리스트 컬렉트 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.lion.wandertrip"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.14.0"
+        versionCode = 15
+        versionName = "1.15.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         signingConfig = signingConfigs.getByName("release")
